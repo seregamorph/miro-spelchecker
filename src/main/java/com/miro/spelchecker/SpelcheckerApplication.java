2fd1bd0079beede3c53e447cb54b7a52ae1dd5d2
Sergey Chernov <sergey.ch@miro.com>
2022-11-15 22:47:34 +0100

    reformat

--- src/main/java/com/miro/spelchecker/SpelcheckerApplication.java
+++ src/main/java/com/miro/spelchecker/SpelcheckerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpelcheckerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpelcheckerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpelcheckerApplication.class, args);
+    }
 
 }
