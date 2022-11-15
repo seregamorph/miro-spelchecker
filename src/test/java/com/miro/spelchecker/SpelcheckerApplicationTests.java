2cba794936889103671fa7cdc9dfe31b528849e2
Sergey Chernov <sergey.ch@miro.com>
2022-11-15 23:20:02 +0100

    LoggingInterceptor

--- src/test/java/com/miro/spelchecker/SpelcheckerApplicationTests.java
+++ src/test/java/com/miro/spelchecker/SpelcheckerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpelcheckerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
