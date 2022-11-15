c730576b4f9f448aff0fa05b062b0b7887f94bed
Sergey Chernov <sergey.ch@miro.com>
2022-11-15 22:18:57 +0100

    Spring Initializr

---
+++ src/main/java/com/miro/spelchecker/SpelcheckerApplication.java
@@ -0,0 +1,13 @@
+package com.miro.spelchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpelcheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpelcheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miro/spelchecker/SpelcheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miro.spelchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpelcheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
