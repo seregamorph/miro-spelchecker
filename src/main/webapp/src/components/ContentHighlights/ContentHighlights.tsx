75109242284696b33945fc3e0a9ee756a7de9f8d
Sergey Nikitin <sergey.n@miro.com>
2022-11-18 16:52:05 +0100

    Add the message text when no suggestions exist

--- src/main/webapp/src/components/ContentHighlights/ContentHighlights.tsx
+++ src/main/webapp/src/components/ContentHighlights/ContentHighlights.tsx
@@ -11,8 +11,8 @@ export const ContentHighlights: FC<Props> = ({ check, children }) => {
     const message = children.slice(check.fromPos, check.toPos);
     const end = children.slice(check.toPos);
     return <>
-        <span>{start}</span>
+        <span className={styles.text}>{start}</span>
         <span className={styles.highlight}>{message}</span>
-        <span>{end}</span>
+        <span className={styles.text}>{end}</span>
     </>
 }
