5d55b941594499dc48986bba8d728c26cc0c3ba1
Sergey Chernov <sergey.ch@miro.com>
2022-11-18 18:56:07 +0100

    SecurityHeadersFilter

---
+++ src/main/java/com/miro/spelchecker/filter/SecurityHeadersFilter.java
@@ -0,0 +1,23 @@
+package com.miro.spelchecker.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class SecurityHeadersFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        filterChain.doFilter(request, response);
+    }
+}
