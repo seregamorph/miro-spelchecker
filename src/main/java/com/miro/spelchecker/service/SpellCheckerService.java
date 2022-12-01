d46c1b6a84444d1207632221e27aa22f34dc11b8
Ayca Ulutoprak <ayca@miro.com>
2022-12-01 16:46:48 +0100

    plain text bugfix

---
+++ src/main/java/com/miro/spelchecker/service/SpellCheckerService.java
@@ -0,0 +1,11 @@
+package com.miro.spelchecker.service;
+
+import com.miro.spelchecker.dto.SpelCheckRequestElement;
+import com.miro.spelchecker.dto.SpelCheckResponseElement;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface SpellCheckerService {
+    List<SpelCheckResponseElement> spellCheck(String language, SpelCheckRequestElement element) throws IOException;
+}
--- src/test/java/com/miro/spelchecker/SpelcheckerApplicationTests.java
+++ src/test/java/com/miro/spelchecker/SpelcheckerApplicationTests.java
@@ -1,13 +1,56 @@
 package com.miro.spelchecker;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.stream.Stream;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpelcheckerApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private static Stream<Arguments> provideEnglishStrings() {
+        return Stream.of(
+                Arguments.of("<b>F</b>ramez 1", 0, 6, 3, 13)
+                , Arguments.of("Framez 1", 0, 6, 0, 6)
+                , Arguments.of("They dont go", 5, 9, 5, 9)
+                , Arguments.of("They dont' go", 5, 10, 5, 14) // because of HTML encode.
+                , Arguments.of("They <p>dont'</p> go", 6, 11, 8, 17) // because of HTML encode.
+        );
+    }
 
     @Test
     void contextLoads() {
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideEnglishStrings")
+    void test(String text, int fromPosPlain, int toPosPlain, int fromPos, int toPos) throws Exception {
+
+        if (this.mockMvc == null) {
+            return;
+        }
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/spellcheck?lang=en-US&&text=" + text))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].fromPosPlain").value(fromPosPlain))
+                .andExpect(jsonPath("$[0].toPosPlain").value(toPosPlain))
+                .andExpect(jsonPath("$[0].fromPos").value(fromPos))
+                .andExpect(jsonPath("$[0].toPos").value(toPos));
+
     }
 
 }
