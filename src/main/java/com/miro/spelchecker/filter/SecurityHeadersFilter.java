3eef4bd072d6bd174d71c86ece5853a367c8fcc7
Sergey Nikitin <sergey.n@miro.com>
2022-12-08 11:32:25 +0100

    Add security headers

--- src/main/java/com/miro/spelchecker/filter/SecurityHeadersFilter.java
+++ src/main/java/com/miro/spelchecker/filter/SecurityHeadersFilter.java
@@ -19,6 +19,11 @@ public class SecurityHeadersFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
         response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
         response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "Content-Type");
+        response.setHeader("Content-Security-Policy", "default-src 'self'; script-src-elem 'self' miro.com; style-src-elem 'self' fonts.googleapis.com; font-src 'self' fonts.gstatic.com; img-src 'self' data:");
+        response.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+        response.setHeader("X-Content-Type-Options", "nosniff");
+        response.setHeader("Referrer-Policy", "no-referrer");
+        response.setHeader("Permissions-Policy", "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()");
         filterChain.doFilter(request, response);
     }
 }
